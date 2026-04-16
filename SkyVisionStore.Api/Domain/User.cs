1bc2e3b68e0b23281bd25e8dd01d97bc4ba12d13
Oleg <kilkilidon@gmail.com>
2026-04-16 18:40:45 +0300

    Add Domain Entity-User

---
+++ SkyVisionStore.Api/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace SkyVisionStore.Api.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatadAT { get; set; }
+    }
+
+}
