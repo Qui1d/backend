20e74f976fa2a521f5889975d43cb13d18346fe8
Qu1d <dima.semenchuk2906@gmail.com>
2026-05-04 11:03:36 +0300

    BusinessLogic structure update

---
+++ SkyVisionStore.BusinessLogic/Functions/Product/ProductFilterFlow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyVisionStore.BusinessLogic.Functions.Product
+{
+    internal class ProductFilterFlow
+    {
+    }
+}
